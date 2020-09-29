211aa7796e14670b12a78825cf3c6f22a1d00b9e
andresGarcia97 <andresgarciatehortua@gmail.com>
2020-09-29 01:05:17 -0500

    estructura inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+class Formula {
+  tiempo: number;
+  resultado: number;
+  constructor(tiempo = 0, resultado = 0) {
+    this.tiempo = tiempo;
+    this.resultado = resultado;
+  }
+}
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'hornoDinamicos2';
+  resultado = 0;
+  tiempo = 0;
+  imprimir: Formula = new Formula();
+  resultados: Formula[] = [];
+
+  T(i: number): number {
+    return i;
+  }
+  V(i: number): number {
+    return i;
+  }
+
+  basico(size = 1): void {
+    size = 10;
+    let interval = null;
+    let ts = 0.1;
+    this.imprimir = new Formula(0, this.T(0) + this.V(0));
+    this.resultados.push(this.imprimir);
+    let i = 1;
+    interval = setInterval(() => {
+      this.imprimir = new Formula(i, this.T(i) + this.V(1) * ts);
+      this.resultados.push(this.imprimir);
+      i++;
+      if (i === size + 1) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
+
