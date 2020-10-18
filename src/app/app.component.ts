817472ce8e72f6a3c2893e7f8127bcec5367b08d
andresGarcia97 <andresgarciatehortua@gmail.com>
2020-10-17 23:56:52 -0500

    Descarga de archivos disponible
    -Se da credito de las librerias usadas en el footer.
    -Se cambia el nombre de algunas variables, para mayor claridad.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
 import { Chart } from 'chart.js';
 import { DataChart } from './clases';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'hornoDinamicos2';
 
   // variables globales
   voltaje = 1;
   ts = 1;
-  // 1/100 segundo
-  tiempoAlentamiento = 10;
+  tiempoAlentamiento = 100;
   maximoIteraciones = 500;
 
   // variables primer ejercicio, sin disipacion
   tituloGraficaBasica = 'grafica1';
   tiempo1 = 0;
-  temperatura1 = 0;
+  temperaturaBasico = 0;
   graficaBasica: any;
   resultados = [];
-  datos = [];
-  interval1 = null;
+  datosBasico = [];
+  intervalBasico = null;
   pausadoBasico = true;
+  apagadoBasico = true;
 
   // variables segundo ejercicio, con disipacion
   titulograficaDisipacion = 'grafica2';
@@ -37,16 +38,18 @@ export class AppComponent {
   u = [this.voltaje, this.voltaje, this.voltaje, this.voltaje];
   tiempo2 = 0;
   referencia2 = 1;
-  interval2 = null;
-  temperatura2 = 0;
-  datos2 = [];
+  intervalDisipacion = null;
+  temperaturaDisipacion = 0;
+  datosDisipacion = [];
   graficaDisipacion: any;
   temperaturaRedondeo = 0;
   pausadoDisipasion = true;
+  apagadoDisipasion = true;
 
   private T(temperatura: number): number {
     return temperatura;
   }
+
   private V(voltaje = 1): number {
     return voltaje;
   }
@@ -62,7 +65,7 @@ export class AppComponent {
           pointBorderColor: 'rgba(0, 0, 0)',
           pointBackgroundColor: 'rgba(255, 255, 255)',
           borderColor: 'rgba(0, 0, 0)',
-          data: this.datos,
+          data: this.datosBasico,
         }],
       },
       options: {
@@ -98,7 +101,7 @@ export class AppComponent {
           pointBorderColor: 'rgba(0, 0, 0)',
           pointBackgroundColor: 'rgba(255, 255, 255)',
           borderColor: 'rgba(0, 0, 0)',
-          data: this.datos2,
+          data: this.datosDisipacion,
         }],
       },
       options: {
@@ -131,28 +134,32 @@ export class AppComponent {
   }
 
   private insercionDatosBasico(): void {
-    this.datos.push(new DataChart(this.tiempo1, this.temperatura1));
-    this.resultados.push(this.temperatura1);
+    this.datosBasico.push(new DataChart(this.tiempo1, this.temperaturaBasico));
+    this.resultados.push(this.temperaturaBasico);
     this.graficaBasica.update();
   }
 
   private insercionDatosDisipacion(): void {
-    this.temperaturaRedondeo = Math.round(this.temperatura2);
-    this.salidaSistema.push(this.temperatura2);
+    this.temperaturaRedondeo = Math.round(this.temperaturaDisipacion);
+    this.salidaSistema.push(this.temperaturaDisipacion);
     this.u.push(this.voltaje);
-    this.datos2.push(new DataChart(this.tiempo2, this.temperatura2));
+    this.datosDisipacion.push(new DataChart(this.tiempo2, this.temperaturaDisipacion));
     this.graficaDisipacion.update();
   }
 
   public apagarBasico(): void {
-    clearInterval(this.interval1);
-    this.interval1 = null;
+    clearInterval(this.intervalBasico);
+    this.pausadoBasico = false;
+    this.intervalBasico = null;
+    this.apagadoBasico = false;
     return;
   }
 
   public apagarDisipacion(): void {
-    clearInterval(this.interval2);
-    this.interval2 = null;
+    clearInterval(this.intervalDisipacion);
+    this.pausadoDisipasion = false;
+    this.intervalDisipacion = null;
+    this.apagadoDisipasion = false;
     return;
   }
 
@@ -165,21 +172,22 @@ export class AppComponent {
   }
 
   public hornoBasico(): void {
+    this.apagadoBasico = true;
     this.pausadoBasico = true;
-    this.datos = [];
+    this.datosBasico = [];
     this.resultados = [];
     this.graficarBasica();
-    this.temperatura1 = 0;
+    this.temperaturaBasico = 0;
     this.tiempo1 = 0;
-    this.temperatura1 = this.V(this.voltaje) * this.ts * 0 + this.T(0);
-    this.resultados.push(this.temperatura1);
+    this.temperaturaBasico = this.V(this.voltaje) * this.ts * 0 + this.T(0);
+    this.resultados.push(this.temperaturaBasico);
     this.insercionDatosBasico();
     this.tiempo1 = 1;
-    this.interval1 = setInterval(() => {
+    this.intervalBasico = setInterval(() => {
       if (this.pausadoBasico) {
         // formula
-        this.temperatura1 = this.V(this.voltaje) * this.ts + this.T(this.resultados[this.tiempo1 - 1]);
-        this.temperatura1 = this.temperaturaMinima(this.temperatura1);
+        this.temperaturaBasico = this.V(this.voltaje) * this.ts + this.T(this.resultados[this.tiempo1 - 1]);
+        this.temperaturaBasico = this.temperaturaMinima(this.temperaturaBasico);
         this.insercionDatosBasico();
         this.tiempo1++;
         if (this.tiempo1 === this.maximoIteraciones) {
@@ -191,21 +199,22 @@ export class AppComponent {
 
   public hornoDisipacion(): void {
     this.pausadoDisipasion = true;
+    this.apagadoDisipasion = true;
     this.salidaSistema = [0, 0, 0, 0];
     this.u = [this.voltaje, this.voltaje, this.voltaje, this.voltaje];
-    this.datos2 = [];
+    this.datosDisipacion = [];
     this.tiempo2 = 4;
-    this.datos2.push(new DataChart(this.tiempo2 - 4, this.salidaSistema[this.tiempo2 - 4]));
-    this.datos2.push(new DataChart(this.tiempo2 - 3, this.salidaSistema[this.tiempo2 - 3]));
-    this.datos2.push(new DataChart(this.tiempo2 - 2, this.salidaSistema[this.tiempo2 - 2]));
-    this.datos2.push(new DataChart(this.tiempo2 - 1, this.salidaSistema[this.tiempo2 - 1]));
+    this.datosDisipacion.push(new DataChart(this.tiempo2 - 4, this.salidaSistema[this.tiempo2 - 4]));
+    this.datosDisipacion.push(new DataChart(this.tiempo2 - 3, this.salidaSistema[this.tiempo2 - 3]));
+    this.datosDisipacion.push(new DataChart(this.tiempo2 - 2, this.salidaSistema[this.tiempo2 - 2]));
+    this.datosDisipacion.push(new DataChart(this.tiempo2 - 1, this.salidaSistema[this.tiempo2 - 1]));
     this.graficarDisipacion();
-    this.interval2 = setInterval(() => {
+    this.intervalDisipacion = setInterval(() => {
       if (this.pausadoDisipasion) {
         // formula
-        this.temperatura2 = this.num1 * this.u[this.tiempo2 - 1] + this.num2 * this.u[this.tiempo2 - 2] +
+        this.temperaturaDisipacion = this.num1 * this.u[this.tiempo2 - 1] + this.num2 * this.u[this.tiempo2 - 2] +
           this.den1 * this.salidaSistema[this.tiempo2 - 1] + this.den2 * this.salidaSistema[this.tiempo2 - 2];
-        this.temperatura2 = this.temperaturaMinima(this.temperatura2);
+        this.temperaturaDisipacion = this.temperaturaMinima(this.temperaturaDisipacion);
         this.insercionDatosDisipacion();
         this.tiempo2++;
         if (this.tiempo2 === this.maximoIteraciones) {
@@ -214,5 +223,49 @@ export class AppComponent {
       }
     }, this.ts * this.tiempoAlentamiento);
   }
+
+  private crearDocumentoExcel(datos: any[], nombre: string): void {
+    const nombreHoja = 'Temp en el tiempo';
+    const nombreArchivoExcel = 'resultados-horno'.concat(nombre);
+    const wb = XLSX.utils.book_new();
+    wb.Props = {
+      Title: 'Resultados',
+      Subject: 'Dinamicos, Horno SCADA',
+      Author: 'Andres Garcia',
+      CreatedDate: new Date()
+    };
+    wb.SheetNames.push(nombreHoja);
+    const wsData = [['tiempo', 'temperatura']];
+
+    datos.forEach((data: { x: any; y: any; }) => {
+      const tiempo = data.x;
+      const temperatura = data.y;
+      wsData.push([tiempo, temperatura]);
+    });
+
+    const ws = XLSX.utils.aoa_to_sheet(wsData);
+    wb.Sheets[nombreHoja] = ws;
+    const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    function s2ab(s): ArrayBuffer {
+      const buf = new ArrayBuffer(s.length); // convert s to arrayBuffer
+      const view = new Uint8Array(buf);  // create uint8array as viewer
+      for (let i = 0; i < s.length; i++) {
+        view[i] = s.charCodeAt(i) & 0xFF;
+      } // convert to octet
+      return buf;
+    }
+    // se descarga el archivo
+    saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), nombreArchivoExcel.concat('.xlsx'));
+  }
+
+  public descargarArchivoSinDisipacion(): void {
+    this.crearDocumentoExcel(this.datosBasico, '-sin-disipacion');
+  }
+
+  public descargarArchivoConDisipacion(): void {
+    this.crearDocumentoExcel(this.datosDisipacion, '-con-disipacion');
+  }
+
 }
 
