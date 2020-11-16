7ac57d2c37099b8b056655448dfd330e77cb6fd8
andresGarcia97 <andresgarciatehortua@gmail.com>
2020-11-16 00:41:01 -0500

    Final terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,13 @@ export class AppComponent {
   voltaje = 1;
   ts = 1;
   tiempoAlentamiento = 10;
+  tempAmbiental = 10;
   referencia = 100;
-  maximoIteraciones = 1200;
+  maximoIteraciones = 1000;
   k = 0.2;
+  n = 10;
+  ti = 25;
+  td = 20;
 
   titulografica = 'grafica';
   num = 1.1;
@@ -22,27 +26,34 @@ export class AppComponent {
   den1 = 1.984;
   den2 = -0.9841;
   salidaSistema = [0, 0, 0, 0];
+  integrales = [0, 0, 0, 0];
+  derivadas = [0, 0, 0, 0];
   voltajeInterno = this.voltaje.valueOf();
   uControl = [this.voltajeInterno, this.voltajeInterno, this.voltajeInterno, this.voltajeInterno];
-  tiempo = 0;
   intervalDisipacion = null;
-  temperaturaDisipacion = 0;
   datosDisipacion = [];
   datosReferencia = [];
   datosError = [];
   graficaDisipacion: any;
-  temperaturaRedondeo = 0;
   pausado = true;
   apagado = true;
+  interrump = false;
+  temperaturaSalida = 0;
+  numberInterrupcion = 0;
+  controlProporcional = 0;
+  temperaturaRedondeo = 0;
+  voltajeRedondeo = 0;
+  errorRedondeo = 0;
+  essRedondeo = 0;
+  integral = 0;
+  derivada = 0;
+  tiempo = 0;
   error = 0;
+  tss = 0;
+  ess = 0;
   mp = 0;
   po = 0;
   vf = 0;
-  tss = 0;
-  ess = 0;
-  tempAmbiental = 10;
-  interrump = false;
-  numberInterrupcion = 0;
 
   private graficar(): void {
     this.graficaDisipacion = new Chart(this.titulografica, {
@@ -99,10 +110,12 @@ export class AppComponent {
   }
 
   private insercionDatos(): void {
+    this.integrales.push(this.integral);
+    this.derivadas.push(this.derivada);
     this.datosReferencia.push(new DataChart(this.tiempo, this.referencia));
-    this.salidaSistema.push(this.temperaturaDisipacion);
+    this.salidaSistema.push(this.temperaturaSalida);
     this.uControl.push(this.voltajeInterno);
-    this.datosDisipacion.push(new DataChart(this.tiempo, this.temperaturaDisipacion));
+    this.datosDisipacion.push(new DataChart(this.tiempo, this.temperaturaSalida));
     this.datosError.push(new DataChart(this.tiempo, this.error));
     this.graficaDisipacion.update();
   }
@@ -115,6 +128,7 @@ export class AppComponent {
     this.pausado = false;
     this.intervalDisipacion = null;
     this.apagado = false;
+    this.limpiarDatos();
     return;
   }
 
@@ -122,20 +136,13 @@ export class AppComponent {
     this.pausado = !this.pausado;
   }
 
-  public horno(): void {
-    this.interrump = false;
-    this.pausado = this.apagado = true;
-    this.voltajeInterno = this.voltaje;
-    this.salidaSistema = [0, 0, 0, 0];
-    this.uControl = [this.voltajeInterno, this.voltajeInterno, this.voltajeInterno, this.voltajeInterno];
+  private limpiarDatos(): void {
     this.datosDisipacion = [];
     this.datosReferencia = [];
     this.datosError = [];
-    this.tiempo = 4;
-    const max = 200;
-    const min = 10;
-    this.numberInterrupcion = Math.round(Math.random() * (max - min) + min);
-    this.mp = this.po = this.tss = this.ess = this.vf = this.error = this.numberInterrupcion = 0;
+  }
+
+  private insercionInicialDatos(): void {
     this.datosError.push(new DataChart(this.tiempo - 4, this.error));
     this.datosError.push(new DataChart(this.tiempo - 3, this.error));
     this.datosError.push(new DataChart(this.tiempo - 2, this.error));
@@ -148,40 +155,74 @@ export class AppComponent {
     this.datosDisipacion.push(new DataChart(this.tiempo - 3, this.salidaSistema[this.tiempo - 3]));
     this.datosDisipacion.push(new DataChart(this.tiempo - 2, this.salidaSistema[this.tiempo - 2]));
     this.datosDisipacion.push(new DataChart(this.tiempo - 1, this.salidaSistema[this.tiempo - 1]));
+  }
+
+  public horno(): void {
+    this.interrump = false;
+    this.pausado = this.apagado = true;
+    this.voltajeInterno = this.voltaje;
+    this.salidaSistema = [0, 0, 0, 0];
+    this.integrales = [0, 0, 0, 0];
+    this.derivadas = [0, 0, 0, 0];
+    this.uControl = [this.voltajeInterno, this.voltajeInterno, this.voltajeInterno, this.voltajeInterno];
+    this.limpiarDatos();
+    this.tiempo = 4;
+    const max = 200;
+    const min = 10;
+    this.numberInterrupcion = Math.round(Math.random() * (max - min) + min);
+    this.mp = this.po = this.tss = this.ess = this.vf = this.error = this.controlProporcional = this.integral = 0;
+    this.derivada = this.voltajeRedondeo = this.errorRedondeo = this.essRedondeo = this.temperaturaSalida = 0;
+    this.insercionInicialDatos();
     this.graficar();
     this.intervalDisipacion = setInterval(() => {
       if (this.pausado) {
         // formula
-        this.temperaturaDisipacion = this.num1 * this.uControl[this.tiempo - 1] + this.num2 * this.uControl[this.tiempo - 2] +
+        this.temperaturaSalida = this.num1 * this.uControl[this.tiempo - 1] + this.num2 * this.uControl[this.tiempo - 2] +
           this.den1 * this.salidaSistema[this.tiempo - 1] + this.den2 * this.salidaSistema[this.tiempo - 2];
-        if (this.interrump) {
-          this.interrumpir();
-        }
-        /*if (this.numberInterrupcion === this.tiempo6) {
-          this.interrumpir();
-        }*/
+        // posible interrupcion
+        this.interrumpir();
+        // calculando error
+        this.error = this.referencia - this.temperaturaSalida;
+        // calculado proporcional
+        this.controlProporcional = this.k * this.error;
+        // calculando integral
+        this.integral = this.integrales[this.tiempo - 1] + ((this.k * this.ts) / this.ti) * this.error;
+        // calculando derivada
+        this.derivada = (1 - (this.n * this.ts) / this.td) * this.derivadas[this.tiempo - 1] -
+          this.k * this.n * (this.temperaturaSalida - this.salidaSistema[this.tiempo - 1]);
+        // cambiando el voltaje
+        this.voltajeInterno = this.controlProporcional + this.integral + this.derivada;
         this.insercionDatos();
+        this.redondeoDatos();
         this.tiempo++;
-        this.temperaturaRedondeo = Math.round(this.temperaturaDisipacion);
-        this.validarReferenncia();
+        this.apagadoAutomatico();
         this.revisarMP();
       }
     }, this.ts * this.tiempoAlentamiento);
   }
 
-  private validarReferenncia(): void {
-    this.error = this.referencia - this.temperaturaRedondeo;
-    const voltaje = this.k * this.error;
-    this.voltajeInterno = Math.round((voltaje + Number.EPSILON) * 100) / 100;
+  private redondeoDatos(): void {
+    this.temperaturaRedondeo = this.redondeo2Cifras(this.temperaturaSalida);
+    this.voltajeRedondeo = this.redondeo2Cifras(this.voltajeInterno);
+    this.errorRedondeo = this.redondeo2Cifras(this.error);
+  }
+
+  private apagadoAutomatico(): void {
     if (this.tiempo === this.maximoIteraciones) {
       this.apagar();
     }
     return;
   }
 
+  private redondeo2Cifras(numero: number): number {
+    return Math.round((numero + Number.EPSILON) * 100) / 100;
+  }
+
   private interrumpir(): void {
-    this.temperaturaDisipacion = this.temperaturaDisipacion + this.tempAmbiental;
-    this.interrump = false;
+    if (this.numberInterrupcion === this.tiempo || this.interrump) {
+      this.temperaturaSalida = this.temperaturaSalida + this.tempAmbiental;
+      this.interrump = false;
+    }
   }
 
   public interrumpirTemp(): void {
@@ -193,15 +234,15 @@ export class AppComponent {
     const valorAnterior = this.salidaSistema[this.tiempo - 2];
     const maximoAlcanzado = this.mp.valueOf();
     if (valorActual > valorAnterior && valorActual > maximoAlcanzado) {
-      this.mp = Math.round(valorActual);
+      this.mp = this.redondeo2Cifras(valorActual);
     }
   }
 
   private calcularPo(): void {
     this.vf = this.salidaSistema[this.tiempo - 1];
-    this.vf = Math.round(this.vf);
+    this.vf = this.redondeo2Cifras(this.vf);
     this.po = (this.mp - this.vf) / this.vf;
-    this.po = Math.round((this.po + Number.EPSILON) * 100) / 100;
+    this.po = this.redondeo2Cifras(this.po);
   }
 
   private calcularTSS(): void {
@@ -210,7 +251,7 @@ export class AppComponent {
     let distancia = 0;
     this.datosDisipacion = this.datosDisipacion.reverse();
     this.datosDisipacion.forEach(element => {
-      if (element.y > vfMenor && vfMayor > element.y && distancia < 10) {
+      if (element.y > vfMenor && vfMayor > element.y && distancia < 20) {
         this.tss = element.x;
         while (distancia > 0) {
           distancia--;
@@ -224,6 +265,7 @@ export class AppComponent {
 
   private calcularESS(): void {
     this.ess = this.referencia - this.vf;
+    this.essRedondeo = this.redondeo2Cifras(this.ess);
   }
 
 }
